89da6646189db87bf1bb487ba296e257f659e753
胡治强 <863932404@qq.com>
2021-03-25 09:38:07 +0800

    Update game.h

--- 三字棋/game.h
+++ 三字棋/game.h
@@ -1,6 +1,8 @@
 #define ROW 3
 #define COL 3
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
 //º¯ÊýµÄÉùÃ÷
 void InitBoard(char board[ROW][COL], int row, int col);
 void DisplayBoard(char board[ROW][COL],int row,int col);
