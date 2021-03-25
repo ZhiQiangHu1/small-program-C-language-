d7bdbd1beba92703b02918d7485a0d234938f4bf
胡治强 <863932404@qq.com>
2021-03-25 09:55:00 +0800

    Update game.h

--- 三字棋/game.h
+++ 三字棋/game.h
@@ -8,3 +8,9 @@ void InitBoard(char board[ROW][COL], int row, int col);
 void DisplayBoard(char board[ROW][COL],int row,int col);
 void playermove(char board[ROW][COL],int row,int col);
 ComputerMove(char board[ROW][COL],int row,int col);
+//告诉我们四种游戏状态
+//玩家赢 ---'*'
+//电脑赢---'#'
+//平局---'Q'
+//继续---'C'
+char IsWin(char board[ROW][COL],int row,int col);
